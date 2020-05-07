bbdc7814accced6d532915eab1fc06527bca085c
Niels Tiben <nielstiben@outlook.com>
2020-05-07 17:31:31 +0200

    Initial files for action client implementation.

---
+++ rcljava/src/main/cpp/org_ros2_rcljava_action_client_ActionClientImpl.cpp
@@ -0,0 +1,40 @@
+// Copyright 2016-2018 Niels Tiben <nielstiben@outlook.com>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <jni.h>
+
+#include <cassert>
+#include <cstdio>
+#include <cstdlib>
+#include <string>
+
+#include "rcl/error_handling.h"
+#include "rcl/node.h"
+#include "rcl/rcl.h"
+#include "rmw/rmw.h"
+
+#include "rcljava_common/exceptions.hpp"
+#include "rcljava_common/signatures.hpp"
+
+#include "org_ros2_rcljava_publisher_PublisherImpl.h"
+
+using rcljava_common::exceptions::rcljava_throw_rclexception;
+using rcljava_common::signatures::convert_from_java_signature;
+using rcljava_common::signatures::destroy_ros_message_signature;
+
+JNIEXPORT void JNICALL
+Java_org_ros2_rcljava_action_client_ActionClientImpl_nativeHello()
+{
+    printf("Hello Action\n");
+}
--- rcljava/src/main/java/org/ros2/rcljava/action_client/ActionClientImpl.java
+++ rcljava/src/main/java/org/ros2/rcljava/action_client/ActionClientImpl.java
@@ -1,7 +1,51 @@
 package org.ros2.rcljava.action_client;
 
+import org.ros2.rcljava.common.JNIUtils;
+import org.ros2.rcljava.interfaces.ActionDefinition;
+import org.ros2.rcljava.node.Node;
+import org.ros2.rcljava.publisher.PublisherImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.ref.WeakReference;
+
 /**
  * @author Niels Tiben
  */
-public class ActionClientImpl {
+public class ActionClientImpl<T extends ActionDefinition> implements ActionClient {
+    private static final Logger logger = LoggerFactory.getLogger(ActionClientImpl.class);
+
+    static {
+        try {
+            JNIUtils.loadImplementation(ActionClientImpl.class);
+        } catch (UnsatisfiedLinkError ule) {
+            logger.error("Native code library failed to load.\n" + ule);
+            System.exit(1);
+        }
+    }
+
+    private final WeakReference<Node> nodeReference;
+
+    /**
+     * An integer that represents a pointer to the underlying ROS2 publisher
+     * structure (rcl_publisher_t).
+     */
+    private long handle;
+
+    /**
+     * The topic to which this publisher will publish messages.
+     */
+    private final String topic;
+
+    public ActionClientImpl(final WeakReference<Node> nodeReference, final long handle, final String topic) {
+        this.nodeReference = nodeReference;
+        this.handle = handle;
+        this.topic = topic;
+    }
+
+    public static native void nativeHello();
+
+    public final void hello(){
+        nativeHello();
+    }
 }
